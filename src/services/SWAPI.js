93ea58d8b79288f0bbd5dac4d54a6e6a443a0528
Ricardo Stefani <ricardostefanisantos@gmail.com>
2017-09-01 00:57:52 -0300

    add new calls in SWAPI

--- src/services/SWAPI.js
+++ src/services/SWAPI.js
@@ -22,4 +22,59 @@ export class SWAPI {
       console.warn(error);
     });
   }
+
+  getFilm(filmId) {
+    return fetch("http://swapi.co/api/films/"+filmId+"/")
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  }
+
+  getPerson(personId) {
+    return fetch("http://swapi.co/api/people/"+personId+"/")
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  }
+
+  getSpecie(specieId) {
+    return fetch("http://swapi.co/api/species/"+specieId+"/")
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  }
+
+  getStarship(starshipId) {
+    return fetch("http://swapi.co/api/starships/"+starshipId+"/")
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  }
+
+  getVehicle(vehicleId) {
+    return fetch("http://swapi.co/api/vehicles/"+vehicleId+"/")
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  }
 }
