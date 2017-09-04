59eaa53a70fff7736efd7fd1e1721e59540dabc0
Ricardo Stefani <ricardostefanisantos@gmail.com>
2017-09-04 20:13:10 -0300

    Clean SWAPI

--- src/infrastructure/SWAPI.js
+++ src/infrastructure/SWAPI.js
@@ -1,27 +1,16 @@
 export class SWAPI {
-  
-  constructor()
-  {
-    this._url = "http://swapi.co/api/";
-  }
-
-  getPlanetsAmount() {
-		return fetch("http://swapi.co/api/planets/")
-		.then((response) => response.json())
-		.then((responseJson) => {
-			return responseJson.count;
-		})
-		.catch((error) => {
-			console.warn(error);
-		});
+	
+	constructor()
+	{
+		this._url = "http://swapi.co/api/";
 	}
 
-  get(method, id = "") {
-    let url = this._url + method;
+	get(method, id = "") {
+		let url = this._url + method;
 
-    if (id) {
-      url += "/" + id;
-    }
+		if (id) {
+			url += "/" + id;
+		}
 
 		return fetch(url)
 		.then((response) => response.json())
@@ -32,117 +21,4 @@ export class SWAPI {
 			console.warn(error);
 		});
 	}
-
-  getFilmsAmount() {
-    return fetch("http://swapi.co/api/films/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      return responseJson.count;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
-
-  getFilm(filmId) {
-    return fetch("http://swapi.co/api/films/"+filmId+"/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      return responseJson;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
-
-  getPeopleAmount() {
-    return fetch("http://swapi.co/api/people/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      return responseJson.count;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
-
-  getPerson(personId) {
-    return fetch("http://swapi.co/api/people/"+personId+"/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      responseJson.filmsLength = responseJson.films.length+ " " + ((responseJson.films.length === 1) ? "film": "films");
-      return responseJson;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
-
-  getSpeciesAmount() {
-    return fetch("http://swapi.co/api/species/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      return responseJson.count;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
-
-  getSpecie(specieId) {
-    return fetch("http://swapi.co/api/species/"+specieId+"/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      responseJson.filmsLength = responseJson.films.length+ " " + ((responseJson.films.length === 1) ? "film": "films");
-      return responseJson;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
-
-  getStarshipAmount() {
-    return fetch("http://swapi.co/api/starships/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      return responseJson.count;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
-
-  getStarship(starshipId) {
-    return fetch("http://swapi.co/api/starships/"+starshipId+"/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      responseJson.filmsLength = responseJson.films.length+ " " + ((responseJson.films.length === 1) ? "film": "films");
-      return responseJson;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
-
-  getVehiclesAmount() {
-    return fetch("http://swapi.co/api/vehicles/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      return responseJson.count;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
-
-  getVehicle(vehicleId) {
-    return fetch("http://swapi.co/api/vehicles/"+vehicleId+"/")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      return responseJson;
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
-  }
 }
